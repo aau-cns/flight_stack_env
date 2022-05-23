a86abbaa553b9e75f43cf6eb826ae80011547d69
Martin Scheiber <martin.scheiber@aau.at>
2022-05-23 15:51:57 +0200

    add: new remote for docker registry and minor fixes

--- rootfs_part/coreenv/flightstack/home/.ros_env.bash
+++ rootfs_part/coreenv/flightstack/home/.ros_env.bash
@@ -1,6 +1,9 @@
 source /opt/ros/$ROS_DISTRO/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
+ETH_IP=$(ifconfig "eth0" 2>/dev/null | grep "inet\\b" | awk '{print $2}' | cut -d : -f2)
+WLAN_IP=$(ifconfig "wlan0" 2>/dev/null | grep "inet\\b" | awk '{print $2}' | cut -d : -f2)
+
 export ROS_HOSTNAME=localhost
 export ROS_IP=localhost
 export ROS_MASTER_URI=http://localhost:11311
